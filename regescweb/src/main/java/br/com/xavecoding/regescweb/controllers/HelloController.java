c06fdf32c5b593d24321ef0d330f7ef236e11a55
Bruna S. Pires <69404377+piresbruna@users.noreply.github.com>
2022-05-12 23:07:12 -0300

    Início com Thymeleaf
    Uso do Thymeleaf para exibir conteúdo na página html (processada do lado servidor).
    Modos de responder as requisições com objetos do Spring e usar de seus atributos.

--- regescweb/src/main/java/br/com/xavecoding/regescweb/controllers/HelloController.java
+++ regescweb/src/main/java/br/com/xavecoding/regescweb/controllers/HelloController.java
@@ -1,16 +1,32 @@
 package br.com.xavecoding.regescweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(){
-
-        //o spring irá renderizar o arquivo templates/hello.html
+    @GetMapping("/hello-servlet")
+    public String hello(HttpServletRequest request){
+        request.setAttribute("nome","Bruna");
         return "hello";
+    }
+
+    @GetMapping("/hello-model")
+    public String hello(Model model){
+        model.addAttribute("nome","Bê");
+        return "hello";
+    }
+    @GetMapping("/hello")
+    public ModelAndView hello(){
+        //modelo associado a uma view, assim recebe o arquivo a ser renderizado camo parâmetro no construtor
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("nome", "Pires");
+        return mv;
     }
 
 }
